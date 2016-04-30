c22277a71ad201ab4d70b57861bf697f32a7ab00
svaccari <vaccari.steven@gmail.com>
2016-04-30 10:42:55 -0700

    added pro builder

--- Assets/Scripts/LevelComponenets/Pillar.cs
+++ Assets/Scripts/LevelComponenets/Pillar.cs
@@ -11,17 +11,25 @@ public class Pillar : MonoBehaviour {
 	void Start () {
 		platform.position = platformPoint.position;
 
+		if (fallAngle < 0) {
+		//	fallAngle = 360 - fallAngle;
+		}
+
 	}
 	
 	void OnTriggerEnter(Collider col){
 		if(col.gameObject.tag == ("Player")){
-			InvokeRepeating("Fall", 0.1f,0.1f);
+			InvokeRepeating("Fall", 0.01f,0.01f);
 		}
 	}
 
 	void Fall () {
 		//pillarRoot.localEulerAngles= new Vector3 (0f, 0f, fallAngle);
-		if (transform.rotation.z < Mathf.Abs (fallAngle)) {
+		if (Mathf.Abs(pillarRoot.localEulerAngles.z) <= Mathf.Abs(fallAngle)) {
+			
+			platform.position = platformPoint.position;
+			Debug.Log (Mathf.Abs (fallAngle));
+			Debug.Log (pillarRoot.localEulerAngles.z);
 			pillarRoot.Rotate (Vector3.forward * fallspeed);
 		}
 	}
